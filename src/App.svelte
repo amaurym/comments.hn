18ca6642ce3268f6d6bba338bd5129bae559db4c
Amaury <1293565+amaurym@users.noreply.github.com>
2021-06-25 22:32:26 +0200

    chore: Cleanup lint, CI and other code fixes (#1)
    * Add lint
    
    * Add ci
    
    * Add license
    
    * Try surge
    
    * Fix surge
    
    * Tweaks
    
    * rename app
    
    * Switch back
    
    * update readme

--- src/App.svelte
+++ src/App.svelte
@@ -1,74 +1,105 @@
 <script lang="ts">
-  import { commentsHn, parseQueryParams } from "./commentsHn";
-  import type { AlgoliaHit } from "./commentsHn";
-  import Hit from "./Hit.svelte";
-  import pkgJSON from "../package.json";
+	import { commentsHn, parseQueryParams } from './commentsHn';
+	import type { AlgoliaHit } from './commentsHn';
+	import Hit from './Hit.svelte';
 
-  const websiteName = pkgJSON.name; // "comments.hn"
-  const parsedQuery = parseQueryParams(location.search);
+	const parsedQuery = parseQueryParams(location.search);
 
-  let hits: AlgoliaHit[] = [];
-  let err: Error;
-  parsedQuery &&
-    commentsHn(parsedQuery)
-      .then((h) => (hits = h))
-      .catch((e) => (err = e));
+	let hits: AlgoliaHit[] = [];
+	let err: Error;
+	parsedQuery &&
+		commentsHn(parsedQuery)
+			.then((h) => (hits = h))
+			.catch((e) => (err = e));
 </script>
 
 <main>
-  <h1>{websiteName}</h1>
-  <p>
-    Preprend <code><mark><strong>{websiteName}?q=</strong></mark></code> to your
-    URL bar to see Hacker News comments about any website.
-  </p>
-  <div>
-    <p>
-      For example, if you're reading Ciechanowski's <a
-        href="https://ciechanow.ski/gears">article</a
-      > on gears, just do:
-    </p>
-    <table>
-      <tbody>
-        <tr>
-          <td><strong><code>URL Bar</code></strong></td>
-          <td>
-            <strong>
-              <code
-                ><mark>{websiteName}?q=</mark>https://ciechanow.ski/gears</code
-              ></strong
-            >
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+	<h1>Comments.hn</h1>
+	<p>
+		Preprend <code><mark><strong>comments.hn?q=</strong></mark></code> to any
+		website to see its Hacker News comments.
+	</p>
+	<div>
+		<p>
+			For example, if you're reading Ciechanowski's <a
+				href="https://ciechanow.ski/gears"
+				rel="noopener noreferrer"
+				target="_blank">article</a
+			> on gears, just do:
+		</p>
+		<table>
+			<tbody>
+				<tr>
+					<td><strong><code>URL Bar</code></strong></td>
+					<td>
+						<strong>
+							<code
+								><mark>comments.hn?q=</mark
+								>https://ciechanow.ski/gears</code
+							></strong
+						>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 
-  {#if !parsedQuery}
-    <br />
-  {:else if err}
-    <h2>Aaaaaahhhhh! An error.</h2>
-    <p>Please create an issue on Github with the following text:</p>
-    <pre>{err.message}</pre>
-  {:else if hits.length}
-    <h2>
-      {hits.length} search results.
-    </h2>
-    <p>Searching for "{parsedQuery}".</p>
-    {#each hits as hit}
-      <Hit {hit} />
-    {/each}
-  {:else}
-    <h2>Loading results...</h2>
-    <p>Searching for "{parsedQuery}".</p>
-  {/if}
+	{#if !parsedQuery}
+		<br />
+	{:else if err}
+		<h2>Aaaaaahhhhh! An error.</h2>
+		<p>
+			Please create an <a
+				href="https://github.com/amaurym/comments.hn/issues"
+				rel="noopener noreferrer"
+				target="_blank">issue</a
+			> on Github with the following text:
+		</p>
+		<pre>{err.message}</pre>
+	{:else if hits.length}
+		<h2>
+			{hits.length} search results.
+		</h2>
+		<p>Searching for "{parsedQuery}".</p>
+		{#each hits as hit}
+			<Hit {hit} />
+		{/each}
+	{:else}
+		<h2>Loading results...</h2>
+		<p>Searching for "{parsedQuery}".</p>
+	{/if}
 
-  <footer>
-    <br />
-    <small
-      ><strong>{websiteName}</strong> is an open-source website with no ads, no
-      trackers, no React. TODOkb gzipped. Its business model is
-      <a>donations</a>. Copyright
-      <a href="https://github.com/amaurym" target="_blank">@amaurym</a> 2021.
-    </small>
-  </footer>
+	<footer>
+		<hr />
+		<p>
+			<small>
+				<strong>Comments.hn</strong> is an
+				<a
+					href="https://github.com/amaurym/comments.hn"
+					rel="noopener noreferrer"
+					target="_blank">open-source</a
+				>
+				website with no ads, no trackers, no React. 64kb gzipped. Its business
+				model is
+				<a
+					href="https://github.com/sponsors/amaurym"
+					rel="noopener noreferrer"
+					target="_blank">donations</a
+				>. Copyright
+				<a
+					href="https://github.com/amaurym"
+					rel="noopener noreferrer"
+					target="_blank">@amaurym</a
+				> 2021.
+			</small>
+		</p>
+		<p>
+			<small>
+				To use this website as a bookmarklet, drag <a
+					href={'javascript:(function(){window.open("https://comments.hn?q="+location.href,"_blank")})()'}
+					>this link</a
+				> to your bookmarks toolbar.
+			</small>
+		</p>
+	</footer>
 </main>
--- src/commentsHn.ts
+++ src/commentsHn.ts
@@ -1,14 +1,14 @@
-import { cleanUrl, askAlgolia } from "./whatHnSays";
+import { askAlgolia, cleanUrl } from './whatHnSays';
 
-export const HN_URL = "https://news.ycombinator.com/item?id=";
+export const HN_URL = 'https://news.ycombinator.com/item?id=';
 
 export interface AlgoliaHit {
-  author: string;
-  created_at_i: number;
-  num_comments: number;
-  objectID: number;
-  points: number;
-  title: string;
+	author: string;
+	created_at_i: number;
+	num_comments: number;
+	objectID: number;
+	points: number;
+	title: string;
 }
 
 /**
@@ -16,19 +16,19 @@ export interface AlgoliaHit {
  * returns the value of the "q=" component, if it exists.
  */
 export function parseQueryParams(queryParams: string): string | undefined {
-  if (!queryParams.startsWith("?")) {
-    return;
-  }
+	if (!queryParams.startsWith('?')) {
+		return;
+	}
 
-  const noQuestionMark = queryParams.substring(1); // Remove "?".
-  const comps = noQuestionMark.split("&");
+	const noQuestionMark = queryParams.substring(1); // Remove "?".
+	const comps = noQuestionMark.split('&');
 
-  // Find the query component that starts with `q=`
-  for (let comp of comps) {
-    if (comp.startsWith("q=")) {
-      return comp.substring(2);
-    }
-  }
+	// Find the query component that starts with `q=`
+	for (const comp of comps) {
+		if (comp.startsWith('q=')) {
+			return comp.substring(2);
+		}
+	}
 }
 
 /**
@@ -37,18 +37,19 @@ export function parseQueryParams(queryParams: string): string | undefined {
  * @param query - The query to search on HN.
  */
 export async function commentsHn(query: string): Promise<AlgoliaHit[]> {
-  const cleaned = cleanUrl(query);
-  if (!cleaned) {
-    return [];
-  }
+	const cleaned = cleanUrl(query);
+	if (!cleaned) {
+		return [];
+	}
 
-  console.log(`Searching on Algolia for query "${cleaned}".`);
+	console.log(`Searching on Algolia for query "${cleaned}".`);
 
-  const { hits } = await askAlgolia(cleaned);
-  console.log(hits);
-  if (hits.length === 1) {
-    location.replace(`${HN_URL}${hits[0].objectID}`);
-  }
+	const { hits } = await askAlgolia(cleaned);
+	console.log(hits);
 
-  return hits;
+	if (hits.length === 1) {
+		location.href = `${HN_URL}${hits[0].objectID}`;
+	}
+
+	return hits;
 }
